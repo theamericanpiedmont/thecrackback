aa720ab912373524792c6f9115242e8e2f8b070f
Rick Newkirk <ricknewkirk@Mac.attlocal.net>
2026-03-25 19:20:32 -0400

    tiniest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
 
   return (
     <main className="min-h-screen">
-      <div className="mx-auto max-w-5xl px-6 py-10">
+      <div className="mx-auto max-w-5xl px-6 py-16">
         <header className="border-b border-black/10 pb-10 dark:border-white/10">
   <div className="flex items-center justify-between">
     <Link
