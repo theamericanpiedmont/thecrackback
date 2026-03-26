becde68dac3756f0e351f5aac5fb75cf20534426
Rick Newkirk <ricknewkirk@Mac.attlocal.net>
2026-03-26 10:59:02 -0400

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   metadataBase: new URL("https://thecrackback.com"),
@@ -29,7 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
